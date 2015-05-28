6665df98e47b9c3f645727bc3e1576a1d6181fc1
Gianluca Pulvirenti <gpulvirenti777@gmail.com>
2015-05-28 22:14:07 +0100

    added css styling and tweets limit to avoid having to scroll page down

--- public/js/app.js
+++ public/js/app.js
@@ -2,8 +2,16 @@ var socket = io();
 
 var tweets = ko.observableArray();
 
+var maxItems = 8;
+var count = 0;
+
 socket.on('tweet', function(data) {
     tweets.push(data.tweet);
+
+    // truncate first items if over 5        
+    if (tweets().length > maxItems) {
+        tweets.shift();
+    }
 });
 
 ko.applyBindings();
