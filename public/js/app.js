6a42f05bbef73fb2d8fd326166cac234c70a4cdf
Harry <harrywinser@hotmail.co.uk>
2015-06-02 20:24:45 +0100

    Should now load in from the top. Also increased the size to 100 tweets.

--- public/js/app.js
+++ public/js/app.js
@@ -2,16 +2,17 @@ var socket = io();
 
 var tweets = ko.observableArray();
 
-var maxItems = 10;
+var maxItems = 100;
 var count = 0;
 
 socket.on('tweet', function(data) {
-    tweets.push(data.tweet);
-
-    // truncate first items if over max number of items        
+    tweets.unshift(data.tweet);
+    // truncate first items if over max number of items
     if (tweets().length > maxItems) {
         tweets.shift();
     }
+
+
 });
 
 ko.applyBindings();
